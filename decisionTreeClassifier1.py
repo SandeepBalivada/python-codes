9a3e26756e1480420eef032b375a002578626dd6
Sandeep Balivada <sandeepbalivada123@gmail.com>
2020-02-27 23:27:46 +0530

    day4

---
+++ decisionTreeClassifier1.py
@@ -0,0 +1,22 @@
+from sklearn import datasets
+from sklearn import metrics
+
+from sklearn.tree import DecisionTreeClassifier
+
+dataset = datasets.load_iris()
+
+print("Hii sandeep\n")
+model = DecisionTreeClassifier()
+print(model)
+
+print("Hii again")
+
+model.fit(dataset.data ,dataset.target)
+
+print(model)
+
+expected = dataset.target
+predicted = model.predict(dataset.data)
+
+print(metrics.classification_report(expected,predicted))
+print(metrics.confusion_matrix(expected,predicted))
